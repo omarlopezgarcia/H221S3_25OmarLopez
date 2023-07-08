81faaa1d938195cc19253d67d1c173c4847c1144
Omar López García <111783739+omarlopezgarcia@users.noreply.github.com>
2023-07-07 22:35:34 -0500

    Maestro 1 y Maestro 2 (Structure y Dates *Acabado)

--- H221S3_25OmarLopez_dates.sql
+++ H221S3_25OmarLopez_dates.sql
@@ -1,76 +1,46 @@
--- DATOS PARA TABLA ESTUDIANTES
+-------------------------------------------------------------------------------------
 -------------------- INSERCIONES
 ---- DERECHOS
-insert into duty (title, dates) values
-('Matricula 1','20-02-2023'),
-('Mensualidad 1','03-03-2023'),
-('Mensualidad 2','03-04-2023'),
-('Mensualidad 3','03-05-2023'),
-('Mensualidad 4','03-06-2023'),
-('Mensualidad 5','03-07-2023'),
-('Matricula 2','01-08-2023'),
-('Mensualidad 1','15-08-2023'),
-('Mensualidad 2','15-09-2023'),
-('Mensualidad 3','15-10-2023'),
-('Mensualidad 4','15-11-2023'),
-('Mensualidad 5','15-12-2023');
+insert into duty (title, dates, amount) values
+('Matricula 1','20-02-2023', '200.00'),
+('Mensualidad Marzo','03-03-2023', '400.00'),
+('Mensualidad Abril','03-04-2023', '400.00'),
+('Mensualidad Mayo','03-05-2023', '400.00'),
+('Mensualidad Junio','03-06-2023', '400.00'),
+('Mensualidad Julio','03-07-2023', '400.00'),
+('Matricula 2','01-08-2023', '200.00'),
+('Mensualidad Agosto','15-08-2023', '400.00'),
+('Mensualidad Setiembre','15-09-2023', '400.00'),
+('Mensualidad Octube','15-10-2023', '400.00'),
+('Mensualidad Noviembre','15-11-2023', '400.00'),
+('Mensualidad Diciembre','15-12-2023', '400.00');
 
 ---- ESTUDIANTES AS
-insert into asstudent (names, last_name, document_type, document_number, career, semester) values
-('Rosendo','Baena Cordoba','DNI','75165597','AS','I'),
-('Claudia','Jurado Carpio','DNI','75162542','AS','I'),
-('Ariel','Landa Garrido','DNI','76007178','AS','II'),
-('Patricia','Cuenca Izaguirre','DNI','73830737','AS','II'),
-('Tania','Alcaraz Bermudez','DNI','75165587','AS','II'),
-('Itziar','Anglada Azorin','DNI','71997493','AS','II'),
-('Pascual','Zabaleta Lombardi','DNI','72893577','AS','III'),
-('Silvio','Verdejo Campusano','DNI','70145327','AS','III'),
-('Ezequiel','Noriega Marques','DNI','75261202','AS','III'),
-('Adelardo','del Novoa Carrión','DNI','70905459','AS','III'),
-('Celestino','Aguila Nicolas','DNI','72308430','AS','III'),
-('Mariana','Cifuentes Frutos','DNI','76787956','AS','III');
+insert into asstudent (names, last_name, document_type, document_number, career, semester, title, amount, active) values
+('Rosendo','Baena Cordoba','DNI','75165597','AS','1', 'Matricula 1', '150.00', 'D'),
+('Claudia','Jurado Carpio','DNI','75162542','AS','1', 'Matricula 1', '150.00', 'D'),
+('Ariel','Landa Garrido','DNI','76007178','AS','2', 'Matricula 1', '150.00', 'D'),
+('Patricia','Cuenca Izaguirre','DNI','73830737','AS','2', 'Matricula 1', '150.00', 'D'),
+('Tania','Alcaraz Bermudez','DNI','75165587','AS','2', 'Matricula 1', '200.00', 'C'),
+('Itziar','Anglada Azorin','DNI','71997493','AS','2', 'Matricula 1', '200.00', 'C'),
+('Pascual','Zabaleta Lombardi','DNI','72893577','AS','3', 'Matricula 1', '200.00', 'D'),
+('Silvio','Verdejo Campusano','DNI','70145327','AS','3', 'Matricula 1', '200.00', 'D'),
+('Ezequiel','Noriega Marques','DNI','75261202','AS','3', 'Matricula 1', '200.00', 'C'),
+('Adelardo','del Novoa Carrión','DNI','70905459','AS','4', 'Matricula 1', '200.00', 'D'),
+('Celestino','Aguila Nicolas','DNI','72308430','AS','4', 'Matricula 1', '200.00', 'C'),
+('Mariana','Cifuentes Frutos','DNI','76787956','AS','5', 'Matricula 2', '200.00', 'C');
 
 ---- ESTUDIANTES PA
-insert into pastudent (names, last_name, document_type, document_number, career, semester) values
-('Claudia','Bravo Reyna','DNI','85142536','PA','I'),
-('Marcos','Palomino Saavedra','DNI','85412578','PA','I'),
-('Rosa','Rodriguez Ovidio','DNI','15359575','PA','II'),
-('Camilo','Mata Campos','DNI','15665544','PA','II'),
-('Manuel','Del Prado Vidal','DNI','11223369','PA','II'),
-('Carolina','Manrique de la Cruz','DNI','88552214','PA','II'),
-('Ezequiel','Santana Pique','DNI','11447756','PA','III'),
-('Martin','Tejada Farfan','DNI','99661147','PA','III'),
-('Gregoria','Alvarado Carmen','DNI','88554412','PA','III'),
-('Luis','Gamarra Solorzano','DNI','33221169','PA','III'),
-('Emilio','Arenas Torres','DNI','22551147','PA','III'),
-('Maria Franchesca','Tenorio Fernandez','DNI','88224413','PA','III');
-
----- PAGO ESTUDIANTES AS
-insert into asstundentpay (amount, payment, asstudent_identifier, duty_identifier) values
-('200.00','C',1,1),
-('100.00','D',2,1),
-('200.00','C',3,1),
-('150.00','D',4,1),
-('200.00','C',5,1),
-('200.00','C',6,1),
-('170.00','D',7,1),
-('200.00','C',8,1),
-('150.00','D',9,1),
-('150.00','D',10,1),
-('200.00','C',11,1),
-('200.00','C',12,1);
-
----- PAGO ESTUDIANTES PA
-insert into pastundentpay (amount, payment, asstudent_identifier, duty_identifier) values
-('100.00','D',1,1),
-('140.00','D',2,1),
-('100.00','D',3,1),
-('200.00','C',4,1),
-('200.00','C',5,1),
-('200.00','C',6,1),
-('170.00','D',7,1),
-('200.00','C',8,1),
-('200.00','C',9,1),
-('150.00','D',10,1),
-('200.00','C',11,1),
-('150.00','D',12,1);
+insert into pastudent (names, last_name, document_type, document_number, career, semester, title, amount, active) values
+('Claudia','Bravo Reyna','DNI','85142536','PA','1', 'Mensualidad Marzo', '400.00', 'C'),
+('Marcos','Palomino Saavedra','DNI','85412578','PA','1', 'Mensualidad Marzo', '400.00', 'C'),
+('Rosa','Rodriguez Ovidio','DNI','15359575','PA','2', 'Mensualidad Marzo', '300.00', 'D'),
+('Camilo','Mata Campos','DNI','15665544','PA','2', 'Mensualidad Marzo', '300.00', 'D'),
+('Manuel','Del Prado Vidal','DNI','11223369','PA','2', 'Mensualidad Marzo', '400.00', 'C'),
+('Carolina','Manrique de la Cruz','DNI','88552214','PA','3', 'Mensualidad Marzo', '200.00', 'D'),
+('Ezequiel','Santana Pique','DNI','11447756','PA','3', 'Mensualidad Marzo', '300.00', 'D'),
+('Martin','Tejada Farfan','DNI','99661147','PA','3', 'Mensualidad Marzo', '300.00', 'D'),
+('Gregoria','Alvarado Carmen','DNI','88554412','PA','4', 'Mensualidad Marzo', '300.00', 'D'),
+('Luis','Gamarra Solorzano','DNI','33221169','PA','4', 'Mensualidad Marzo', '400.00', 'C'),
+('Emilio','Arenas Torres','DNI','22551147','PA','5', 'Mensualidad Marzo', '100.00', 'D'),
+('Maria Franchesca','Tenorio Fernandez','DNI','88224413','PA','5', 'Mensualidad Marzo', '400.00', 'C');
--- H221S3_25OmarLopez_structure.sql
+++ H221S3_25OmarLopez_structure.sql
@@ -26,12 +26,14 @@ CREATE TABLE asstudent (
 			document_number LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' OR 
 			document_number LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') NOT NULL,
     career char(2) check(career = 'AS') NOT NULL,
-    semester char(3) check(semester = 'I' OR
-						semester = 'II' OR
-						semester = 'III' OR
-						semester = 'IV' OR
-						semester = 'V') NOT NULL,
-    active char(1) check(active = 'A' or active = 'I') DEFAULT('A') NOT NULL,
+    semester char(1) check(semester = '1' OR
+						semester = '2' OR
+						semester = '3' OR
+						semester = '4' OR
+						semester = '5') NOT NULL,
+	title varchar(26) NOT NULL,
+	amount float NOT NULL,
+    active char(1) check(active = 'D' or active = 'C') NOT NULL,
     CONSTRAINT asstudent_pk PRIMARY KEY  (identifier)
 );
 
@@ -46,67 +48,32 @@ CREATE TABLE pastudent (
 			document_number LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' OR 
 			document_number LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') NOT NULL,
     career char(2) check(career = 'PA') NOT NULL,
-    semester char(3) check(semester = 'I' OR
-						semester = 'II' OR
-						semester = 'III' OR
-						semester = 'IV' OR
-						semester = 'V') NOT NULL,
-    active char(1) check(active = 'A' or active = 'I') DEFAULT('A') NOT NULL,
+    semester char(1) check(semester = '1' OR
+						semester = '2' OR
+						semester = '3' OR
+						semester = '4' OR
+						semester = '5') NOT NULL,
+	title varchar(26) NOT NULL,
+	amount float NOT NULL,
+    active char(1) check(active = 'D' or active = 'C') NOT NULL,
     CONSTRAINT pastudent_pk PRIMARY KEY  (identifier)
 );
 
 -- Table: duty
 CREATE TABLE duty (
     identifier int IDENTITY(1,1) NOT NULL,
-    title varchar(20)  NOT NULL,
+    title varchar(26)  NOT NULL,
     dates date  NOT NULL,
+	amount float NOT NULL,
+	active char(1) check(active = 'A' or active = 'I') DEFAULT('A') NOT NULL,
     CONSTRAINT duty_pk PRIMARY KEY  (identifier)
 );
 
--- Table: asstudentpay
-CREATE TABLE asstudentpay (
-    identifier int IDENTITY(1,1) NOT NULL,
-	amount decimal(8,2)  NOT NULL,
-    payment char(1) check(payment = 'D' or payment = 'C') NOT NULL,
-    asstudent_identifier int  NOT NULL,
-    duty_identifier int  NOT NULL,
-    CONSTRAINT asstudentpay_pk PRIMARY KEY  (identifier)
-);
-
--- Table: pastudentpay
-CREATE TABLE pastudentpay (
-    identifier int IDENTITY(1,1) NOT NULL,
-    amount decimal(8,2)  NOT NULL,
-    payment char(1) check(payment = 'D' or payment = 'C') NOT NULL,
-    pastudent_identifier int  NOT NULL,
-    duty_identifier int  NOT NULL,
-    CONSTRAINT pastudentpay_pk PRIMARY KEY  (identifier)
-);
---------------------------------------------------------------------------
--- foreign keys
--- Reference: asstudentdebt_asstudent (table: asstudentpay)
-ALTER TABLE asstudentpay ADD CONSTRAINT asstudentdebt_asstudent
-    FOREIGN KEY (asstudent_identifier)
-    REFERENCES asstudent (identifier);
-
--- Reference: asstudentdebt_duty (table: asstudentpay)
-ALTER TABLE asstudentpay ADD CONSTRAINT asstudentdebt_duty
-    FOREIGN KEY (duty_identifier)
-    REFERENCES duty (identifier);
-
--- Reference: pastudentpay_duty (table: pastudentpay)
-ALTER TABLE pastudentpay ADD CONSTRAINT pastudentpay_duty
-    FOREIGN KEY (duty_identifier)
-    REFERENCES duty (identifier);
-
--- Reference: pastudentpay_pastudent (table: pastudentpay)
-ALTER TABLE pastudentpay ADD CONSTRAINT pastudentpay_pastudent
-    FOREIGN KEY (pastudent_identifier)
-    REFERENCES pastudent (identifier);
 --------------------------------------------------------------------------------
------ TABLA VISTA Y CADA UNA CON SU INSERCIÃ“N
+
+----- TABLA VISTA Y CADA UNA CON SU INSERCIÓN
 -- VISTA AS STUDENT ACTIVE
-CREATE VIEW asstudent_active
+CREATE VIEW asstudent_debt
 	AS
 	SELECT
 		identifier,
@@ -116,12 +83,14 @@ CREATE VIEW asstudent_active
 		document_number,
 		career,
 		semester,
+		title,
+		amount,
 		active
 	FROM asstudent
-	WHERE active='A'
+	WHERE active='D'
 GO
 -- VISTA AS STUDENT INACTIVE
-CREATE VIEW asstudent_inactive
+CREATE VIEW asstudent_cancelled
 	AS
 	SELECT
 		identifier,
@@ -131,13 +100,15 @@ CREATE VIEW asstudent_inactive
 		document_number,
 		career,
 		semester,
+		title,
+		amount,
 		active
 	FROM asstudent
-	WHERE active='I'
+	WHERE active='C'
 GO
 
 -- VISTA PA STUDENT ACTIVE
-CREATE VIEW pastudent_active
+CREATE VIEW pastudent_debt
 	AS
 	SELECT
 		identifier,
@@ -147,12 +118,14 @@ CREATE VIEW pastudent_active
 		document_number,
 		career,
 		semester,
+		title,
+		amount,
 		active
 	FROM pastudent
-	WHERE active='A'
+	WHERE active='D'
 GO
 -- VISTA PA STUDENT INACTIVE
-CREATE VIEW pastudent_inactive
+CREATE VIEW pastudent_cancelled
 	AS
 	SELECT
 		identifier,
@@ -162,81 +135,72 @@ CREATE VIEW pastudent_inactive
 		document_number,
 		career,
 		semester,
+		title,
+		amount,
 		active
 	FROM pastudent
-	WHERE active='I'
+	WHERE active='C'
 GO
-----------------------------------
--- VISTA AS STUDENT DEBT
-CREATE VIEW asstudentpay_debt
+
+-- VISTA PARA DERECHOS
+CREATE VIEW duty_date
 	AS
 	SELECT
-		p.identifier,
-		CONCAT(a.names,' ',UPPER(a.last_name)) AS 'alumnoAS',
-		a.career,
-		a.semester,
-		d.title AS 'derecho',
-		p.amount,
-		p.payment
-	FROM asstudentpay AS p
-	INNER JOIN asstudent AS a ON p.asstudent_identifier = a.identifier
-	INNER JOIN duty AS d ON p.duty_identifier = d.identifier
-	WHERE payment='D'
-GO
--- VISTA AS STUDENT CANCELLED
-CREATE VIEW asstudentpay_cancelled
-	AS
-	SELECT
-		p.identifier,
-		CONCAT(a.names,' ',UPPER(a.last_name)) AS 'alumnoAS',
-		a.career,
-		a.semester,
-		d.title AS 'derecho',
-		p.amount,
-		p.payment
-	FROM asstudentpay AS p
-	INNER JOIN asstudent AS a ON p.asstudent_identifier = a.identifier
-	INNER JOIN duty AS d ON p.duty_identifier = d.identifier
-	WHERE payment='C'
-GO
--- VISTA PA STUDENT DEBT
-CREATE VIEW pastudentpay_debt
-	AS
-	SELECT
-		p.identifier,
-		CONCAT(pa.names,' ',UPPER(pa.last_name)) AS 'alumnoAS',
-		pa.career,
-		pa.semester,
-		d.title AS 'derecho',
-		p.amount,
-		p.payment
-	FROM pastudentpay AS p
-	INNER JOIN pastudent AS pa ON p.pastudent_identifier = pa.identifier
-	INNER JOIN duty AS d ON p.duty_identifier = d.identifier
-	WHERE payment='D'
-GO
--- VISTA PA STUDENT CANCELLED
-CREATE VIEW pastudentpay_cancelled
-	AS
-	SELECT
-		p.identifier,
-		CONCAT(pa.names,' ',UPPER(pa.last_name)) AS 'alumnoAS',
-		pa.career,
-		pa.semester,
-		d.title AS 'derecho',
-		p.amount,
-		p.payment
-	FROM pastudentpay AS p
-	INNER JOIN pastudent AS pa ON p.pastudent_identifier = pa.identifier
-	INNER JOIN duty AS d ON p.duty_identifier = d.identifier
-	WHERE payment='C'
+		identifier,
+		title,
+		FORMAT(dates,'dd/MM') dates,
+		amount
+	FROM duty WHERE active='A'
 GO
 -------------------------------------------------------------------------------------
-select * from asstudent_active;
-select * from asstudent_inactive;
-select * from pastudent_active;
-select * from pastudent_inactive;
-select * from asstudentpay_debt;
-select * from asstudentpay_cancelled;
-select * from pastudentpay_debt;
-select * from pastudentpay_cancelled;
+-------------------- INSERCIONES
+---- DERECHOS
+insert into duty (title, dates, amount) values
+('Matricula 1','20-02-2023', '200.00'),
+('Mensualidad Marzo','03-03-2023', '400.00'),
+('Mensualidad Abril','03-04-2023', '400.00'),
+('Mensualidad Mayo','03-05-2023', '400.00'),
+('Mensualidad Junio','03-06-2023', '400.00'),
+('Mensualidad Julio','03-07-2023', '400.00'),
+('Matricula 2','01-08-2023', '200.00'),
+('Mensualidad Agosto','15-08-2023', '400.00'),
+('Mensualidad Setiembre','15-09-2023', '400.00'),
+('Mensualidad Octube','15-10-2023', '400.00'),
+('Mensualidad Noviembre','15-11-2023', '400.00'),
+('Mensualidad Diciembre','15-12-2023', '400.00');
+
+---- ESTUDIANTES AS
+insert into asstudent (names, last_name, document_type, document_number, career, semester, title, amount, active) values
+('Rosendo','Baena Cordoba','DNI','75165597','AS','1', 'Matricula 1', '150.00', 'D'),
+('Claudia','Jurado Carpio','DNI','75162542','AS','1', 'Matricula 1', '150.00', 'D'),
+('Ariel','Landa Garrido','DNI','76007178','AS','2', 'Matricula 1', '150.00', 'D'),
+('Patricia','Cuenca Izaguirre','DNI','73830737','AS','2', 'Matricula 1', '150.00', 'D'),
+('Tania','Alcaraz Bermudez','DNI','75165587','AS','2', 'Matricula 1', '200.00', 'C'),
+('Itziar','Anglada Azorin','DNI','71997493','AS','2', 'Matricula 1', '200.00', 'C'),
+('Pascual','Zabaleta Lombardi','DNI','72893577','AS','3', 'Matricula 1', '200.00', 'D'),
+('Silvio','Verdejo Campusano','DNI','70145327','AS','3', 'Matricula 1', '200.00', 'D'),
+('Ezequiel','Noriega Marques','DNI','75261202','AS','3', 'Matricula 1', '200.00', 'C'),
+('Adelardo','del Novoa Carrión','DNI','70905459','AS','4', 'Matricula 1', '200.00', 'D'),
+('Celestino','Aguila Nicolas','DNI','72308430','AS','4', 'Matricula 1', '200.00', 'C'),
+('Mariana','Cifuentes Frutos','DNI','76787956','AS','5', 'Matricula 2', '200.00', 'C');
+
+---- ESTUDIANTES PA
+insert into pastudent (names, last_name, document_type, document_number, career, semester, title, amount, active) values
+('Claudia','Bravo Reyna','DNI','85142536','PA','1', 'Mensualidad Marzo', '400.00', 'C'),
+('Marcos','Palomino Saavedra','DNI','85412578','PA','1', 'Mensualidad Marzo', '400.00', 'C'),
+('Rosa','Rodriguez Ovidio','DNI','15359575','PA','2', 'Mensualidad Marzo', '300.00', 'D'),
+('Camilo','Mata Campos','DNI','15665544','PA','2', 'Mensualidad Marzo', '300.00', 'D'),
+('Manuel','Del Prado Vidal','DNI','11223369','PA','2', 'Mensualidad Marzo', '400.00', 'C'),
+('Carolina','Manrique de la Cruz','DNI','88552214','PA','3', 'Mensualidad Marzo', '200.00', 'D'),
+('Ezequiel','Santana Pique','DNI','11447756','PA','3', 'Mensualidad Marzo', '300.00', 'D'),
+('Martin','Tejada Farfan','DNI','99661147','PA','3', 'Mensualidad Marzo', '300.00', 'D'),
+('Gregoria','Alvarado Carmen','DNI','88554412','PA','4', 'Mensualidad Marzo', '300.00', 'D'),
+('Luis','Gamarra Solorzano','DNI','33221169','PA','4', 'Mensualidad Marzo', '400.00', 'C'),
+('Emilio','Arenas Torres','DNI','22551147','PA','5', 'Mensualidad Marzo', '100.00', 'D'),
+('Maria Franchesca','Tenorio Fernandez','DNI','88224413','PA','5', 'Mensualidad Marzo', '400.00', 'C');
+
+select * from asstudent_debt;
+select * from asstudent_cancelled;
+select * from pastudent_debt;
+select * from pastudent_cancelled;
+select * from duty_date;
